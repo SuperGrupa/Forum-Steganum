278b3c66e851c9c2797e52a7de3fc3f202b963aa
Daniel Koza <dakolech@gmail.com>
2015-10-25 18:18:31 +0100

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
 set timeout 1000
 set email "$env(METEOR_EMAIL)"
 set password "$env(METEOR_PASSWORD)"
 
-cd app
+if [ "$TRAVIS_BRANCH" == "develop" ]; then
 
-spawn meteor deploy $env(METEOR_DEPLOY_URL)
+  spawn meteor deploy $env(METEOR_DEPLOY_URL)
 
-#puts $command
-expect -- "*Email:"
-send "$email\n"
-expect -- "*assword:"
-send -- "$password\n"
-expect -- "*Now serving at $env(METEOR_DEPLOY_URL)"
+  expect -- "*Email:"
+  send "$email\n"
+  expect -- "*assword:"
+  send -- "$password\n"
+  expect -- "*Now serving at $env(METEOR_DEPLOY_URL)"
+
+fi
