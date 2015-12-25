361608ca14a1c25e349a348511776bed8aa3a3b4
Bartosz Kostaniak <bartosz@localhost.localdomain>
2015-12-25 19:51:17 +0100

    Ustawianie ziarna 1024 bitowego jako 16 liczb 64 bitowych (z tablicy 32 liczb 32 bitowych)

--- .stegano/js/algorithm/random.js
+++ .stegano/js/algorithm/random.js
@@ -1,8 +1,18 @@
 stegano.algorithm.random = (function () { 
-    var seed;
+    var seed = [];
     
+    // newSeed powinno być tablicą 32 elementową (32 liczby x 32 bity każda = 1024 bity ziarna-klucza)
     function setSeed(newSeed) {
-        seed = newSeed;
+        // test czy newSeed jest tablicą 32-elementową
+        if (Object.prototype.toString.call(newSeed) === Object.prototype.toString.call([]) &&
+            newSeed.length >= 32)
+        {
+            for (var i = 0; i < 16; ++i) {
+                seed.push(dcodeIO.Long.fromBits(newSeed[2*i], newSeed[2*i + 1], true));
+            }
+        } else {
+            throw "Seed must be an array with at least 32 integer values!";
+        }
     }
     
     function getNextNumber(min, max) {
