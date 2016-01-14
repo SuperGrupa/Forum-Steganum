d20b122840706dfb031b55116117193d10373cc8
Daniel Koza <dakolech@gmail.com>
2016-01-14 22:29:10 +0100

    ES6 in posts

--- tests/e2e/lib/main.class.js
+++ tests/e2e/lib/main.class.js
@@ -1,12 +1,16 @@
 'use strict';
 
 class MainClass {
+  get sections() {
+    return element.all(By.repeater('section in listDir.sections'));
+  }
+
   get topics() {
     return element.all(By.repeater('topic in showSectionCtrl.topics'));
   }
 
-  get sections() {
-    return element.all(By.repeater('section in listDir.sections'));
+  get posts() {
+    return element.all(By.repeater('post in listDir.posts'));
   }
 
   inputOn(inputElement, text) {
--- tests/e2e/main.scenario.js
+++ tests/e2e/main.scenario.js
@@ -1,9 +1,9 @@
 'use strict';
 var sections = require('./sections/sections.scenario');
 var topics = require('./topics/topics.scenario');
-// var posts = require('./posts/posts.scenario');
+var posts = require('./posts/posts.scenario');
 
 sections.run();
 topics.run();
-// posts.run();
+posts.run();
 
